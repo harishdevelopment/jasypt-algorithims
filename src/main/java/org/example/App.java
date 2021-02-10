a2afa8575b07cf94e8b50f665ef783ec1e11c1b5
Harish Kumar <harishkumar.ie@gmail.com>
2021-02-10 00:52:34 +0000

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.registry.AlgorithmRegistry;
+
+import java.security.Security;
+import java.util.Set;
+
+public class App {
+  public static void main(String[] args) {
+    try {
+      Security.addProvider(new BouncyCastleProvider());
+      final Set pbeAlgos = AlgorithmRegistry.getAllPBEAlgorithms();
+      System.out.println("#### PBE ALGORITHMS ####");
+      for (Object algorithms : pbeAlgos){
+        System.out.println(algorithms);
+      }
+    } catch (Throwable t) {
+      t.printStackTrace(System.err);
+    }
+  }
+}
